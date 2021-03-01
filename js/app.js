379b63b26dcd256ff1aff3b28d45ed43704eea0a
mohsmadi <msmadi_7@yahoo.com>
2021-03-01 23:21:44 +0200

    lab About Me

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let str = "Hello Clint!";
+let  = str.toLocaleUpperCase();
+
+let clintName = prompt("Please enter your name", "Mohammad");
+
+if (clintName != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + clintName + "! How are you today?";
+}
+
+
+let name=prompt("Clint Name");
+alert (" hello + name");
+
+let order1=prompt('enter price of order 1')
+let order2=prompt('enter price of order 2')
+alert ( order1+order2)
+
+
+
+
+
+
+
+
+
+
+let str = "Thank You!";
+let res = str.toLowerCase();
