8fd5b282a2ef0d8a7ef0b42bf1ff4057460eaba0
mohsmadi <msmadi_7@yahoo.com>
2021-03-03 20:37:55 +0200

    lab02

--- js/app.js
+++ js/app.js
@@ -1,100 +1,84 @@
-'use strict'
+'use strict';
 
-let Ans;
-let r = confirm("Warm Greetings!");
-if (r == true) {
-  Ans = "You pressed OK!";
-} else {
-  Ans = "You pressed Cancel!";
-}
-
-//let userName = prompt("Please enter your name", "Mohammad!");
-
-//if (userName != 0) {
- // document.getElementById("demo").innerHTML =
- // "Hello " + userName + "! See You Around?";
-//}
-
-let YourName = prompt("What's your Namer?");
-
-if (YourName.toLowerCase() =="Yes") {
-  alert("Wow! Great to See YOU Here!");
-}
+let userName = prompt("Your Name ?");
+alert(" MOST WELCOME " + userName)
 
 
-let favCare = prompt("What's Favourit Care?");
-
-if (favCare.toLowerCase() =="Yes") {
-  alert("Wow! I like it TOO!");
-}
-
-let favPhone = prompt("What's Favourit Phone?");
-
-if (favCare.toLowerCase() =="Yes") {
-  alert("It's So Cool!");
-}
-
-function myFunction() {
-  let greeting;
-  let time = new Date().getHours();
-  if (time < 10) {
-    greeting = "Good morning";
-  } else if (time < 20) {
-    greeting = "Good day";
-  } else {
-    greeting = "Good evening";
-  }
-  document.getElementById("demo").innerHTML = greeting;
-}
-
-let msg;
-let favBook = prompt("What's your favorite Book?");
-switch(favBook) {
-  case "The Cold Millions":
-    msg = "Excellent choice! The Cold Millions is Incredible Book.";
-    break;
-  case "Vesper Flights":
-    msg = "Vesper Flights is my favorite as Will!";
-    break;
-  case "Writers & Lovers":
-    msg = "Really? Are you sure the Writers & Lovers is your favorite?";
-    break;
-  default:
-    msg = "I have never heard of that one..";
-    break;
+let questionOne= prompt ( 'Do You Like summer');
+    questionOne=questionOne.toUpperCase()
+let count_no=0
+let count_yes=0
+console.log(questionOne)
+if (questionOne==='No'|| questionOne==='N')
+   {
+    alert ('Its! Great');
+    count_no=count_no+1
+  } 
+else {   console.log( 'Oops!' );
+alert('You Dont Got It');
+count_yes=count_yes+1
 }
 
 
-if (window.confirm("Do you really want to leave?")) {
-  window.open("exit.html", "Thanks for Visiting!");
+let   questionTwo= prompt ( 'Do You Like Move')
+  questionTwo=questionTwo.toUpperCase()
+  if (questionTwo==='Yes'|| questionTwo==='Y')
+   {
+    console.log ('Yeah! Me Too!');
+    alert ('You Got It');
+    count_yes=count_yes+1
+  } 
+else { 
+  count_no=count_no+1
+alert('You Dont Got It');
 }
 
-    // random value generated 
-    var y = Math.floor(Math.random() * 10 + 1); 
-      
-    // counting the number of guesses 
-    // made for correct Guess 
-    var guess = 1; 
-      
-    document.getElementById("submitguess").onclick = function(){ 
-      
-   // number guessed by user      
-   var x = document.getElementById("guessField").value; 
-  
-   if(x == y) 
-   {     
-       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
-               + guess + " GUESS "); 
-   } 
-   else if(x > y) /* if guessed number is greater 
-                   than actual number*/ 
-   {     
-       guess++; 
-       alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
-   } 
-   else
-   { 
-       guess++; 
-       alert("OOPS SORRY!! TRY A GREATER NUMBER") 
-   } 
-} 
+
+let   questionThree= prompt ( 'Do You Like RealMadrid');
+  questionThree=questionThree.toUpperCase()
+
+  if (questionThree==='Yes'|| questionThree==='Y')
+   {
+    console.log ('Yeah! Me Too!');
+    alert ('You Got It');
+    count_yes=count_yes+1
+  } 
+else {   console.log( 'Oops!' );
+alert('You Dont Got It');
+count_no=count_no+1
+}
+
+
+let   questionFour= prompt ( 'Do You Like Pizza');
+  questionFour=questionFour.toUpperCase()
+
+  if (questionFour==='yes'|| questionFour==='y')
+   {
+    console.log ('Yeah! Me Too!');
+    alert ('You Got It');
+    count_yes=count_yes+1
+    
+  } 
+else {   console.log( 'Oops!');
+alert('You Dont Got It');
+count_no=count_no+1
+}
+
+
+let   questionFive= prompt ( 'Do You Like Transportaion');
+  questionFive=questionFive.toUpperCase()
+  if (questionFive==='NO'|| questionFive==='N')
+   {
+    console.log ('Yeah! Me Too!');
+    alert ('You Got It');
+    count_no=count_no+1
+  } 
+else {   console.log( 'Oops!');
+count_yes=count_yes+1
+alert('You Dont Got It');
+}
+
+
+console.log("count_yes",count_yes)
+console.log("count_no",count_no)
+
